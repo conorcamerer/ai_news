0f628b5fa78872a9bf66c3734f830b512d1c3ae3
conorcamerer <86749267+conorcamerer@users.noreply.github.com>
2022-06-01 16:30:31 -0700

    Reworked infinite load + added randomness

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { createClient } from '@supabase/supabase-js'
-import { useIntersection } from '/lib/useIntersection'
+import handleViewport from 'react-in-viewport'
 import Article from '/components/Article'
 
 const supabase = createClient(
@@ -9,24 +9,38 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
+
+
+const Block = ({ inViewport, forwardedRef }) => {
+  const text = inViewport ? 'In viewport' : 'Not in viewport'
+  return (
+    <div ref={forwardedRef} className="relative w-screen h-screen p-6 sec">
+      <div className="absolute flex gap-4 text-neutral-700 font-bold text-xl font-space">
+        <small className="flicker">
+          Generated{ text } on&nbsp;<div className="inline-block bg-neutral-900 rounded animate-pulse w-32">&nbsp;</div>
+          <br/>
+          _ref:&nbsp;<div className="inline-block bg-neutral-900 rounded animate-pulse w-14">&nbsp;</div>
+        </small>
+      </div>
+      <div className="mt-28 flex flex-col items-center">
+        <div className="inline-block bg-neutral-900 rounded animate-pulse w-72 text-4xl">&nbsp;</div>
+      </div>
+    </div>
+  )
+}
+
+const ViewportBlock = handleViewport(Block)
+
+
 export default function Index() {
   const [articles, setArticles] = useState([])
-  const lastSlide = useRef()
-  const inViewport = useIntersection(lastSlide)
-
-  if (inViewport === true) {
-    getArticle()
-  }
-
 
   async function getArticle() {
-    const maxEdge = articles.length
-    const { data, error } = await supabase.from('articles').select().range(maxEdge, maxEdge+1)
-    if (maxEdge === 0) {
-      setArticles(data)
-    } else {
-      setArticles([...articles, ...data])
-    }
+    console.log('loading data')
+
+    const { data, error } = await supabase.rpc('ts_2r2')
+    setArticles([...articles, data])
+    console.log(articles)
   }
 
 
@@ -45,23 +59,12 @@ export default function Index() {
           <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
         </motion.div>
       </motion.div>
-
-      {articles && articles.map((article, index) => (
+      
+      {articles.length!==0 && articles.map((article, index) => (
         <Article key={index} article={article} index={index} />
       ))}
 
-      <div ref={lastSlide} className="relative w-screen h-screen p-6 sec">
-        <div className="absolute flex gap-4 text-neutral-700 font-bold text-xl font-space">
-          <small className="flicker">
-            Generated on&nbsp;<div className="inline-block bg-neutral-900 rounded animate-pulse w-32">&nbsp;</div>
-            <br/>
-            _ref:&nbsp;<div className="inline-block bg-neutral-900 rounded animate-pulse w-14">&nbsp;</div>
-          </small>
-        </div>
-        <div className="mt-28 flex flex-col items-center">
-          <div className="inline-block bg-neutral-900 rounded animate-pulse w-72 text-4xl">&nbsp;</div>
-        </div>
-      </div>
+      <ViewportBlock onEnterViewport={() => getArticle()} onLeaveViewport={() => console.log('loaded data')} />
 
     </main>
   )
